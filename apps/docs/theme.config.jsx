c439adbdaba32b71c2dd2162a88ff8108d0890f2
Will <willliuwillliu@gmail.com>
2023-01-16 23:04:30 -0500

    Changed package name to docs

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -8,4 +8,5 @@ export default {
   toc: {
     extraContent: <Fragment></Fragment>,
   },
+  // ...
 };
--- packages/ui/src/ThemePicker.tsx
+++ packages/ui/src/ThemePicker.tsx
@@ -104,7 +104,7 @@ export const ThemePicker = () => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6"
+          className="w-4 h-4"
         >
           <path
             strokeLinecap="round"
