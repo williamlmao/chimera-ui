cc789dd20d8b0c28e8dd5800a596e740b0b1ba2f
Will <willliuwillliu@gmail.com>
2023-02-20 11:05:05 -0500

    Fixed tailwind config purging component styles

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   important: true,
-  jit: true,
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,18 +11,8 @@ module.exports = {
     "./nextra-theme-docs/**/*.{js,tsx}",
     "./theme.config.jsx",
     "../../packages/ui/**/*{.js,.ts,.jsx,.tsx}",
-    "../../packages/@chimera-ui/tw-plugin/**/*{.js,.ts,.jsx,.tsx}",
+    "../../packages/components/**/*{.js,.ts,.jsx,.tsx,.mjs}",
   ],
-  theme: {
-    extend: {
-      transitionProperty: {
-        width: "width",
-      },
-      colors: {
-        "base-content-2": "var(--base-content-2)",
-      },
-    },
-  },
   plugins: [
     require("@tailwindcss/container-queries"),
     require("@chimera-ui/tw-plugin"),
