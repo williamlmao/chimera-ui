59bf2787f1cd20c5648c68561177ac0db2211ac2
Will <willliuwillliu@gmail.com>
2023-02-20 11:07:31 -0500

    Styled landing page

--- apps/docs/components/Introduction.tsx
+++ apps/docs/components/Introduction.tsx
@@ -27,7 +27,7 @@ const features = [
     ),
   },
   {
-    title: "Responsive",
+    title: "Free",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -40,13 +40,18 @@ const features = [
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+          d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
+        />
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 6h.008v.008H6V6z"
         />
       </svg>
     ),
   },
   {
-    title: "Themable",
+    title: "Color System Included",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -102,14 +107,33 @@ const features = [
       </svg>
     ),
   },
+  {
+    title: "Theme Generator",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+        />
+      </svg>
+    ),
+  },
 ];
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center mt-24 max-w-[900px]">
-      <div className="text-5xl font-extrabold">
+    <div className="flex flex-col items-center justify-center text-center mt-8 md:mt-24 max-w-[900px]">
+      <div className="text-2xl md:font-extrabold md:text-5xl font-bold">
         {
-          "Fully customizable components that you probably won't need to customize"
+          "Beautiful, accessible, and fully customizable React + Tailwind components"
         }
       </div>
     </div>
@@ -118,19 +142,23 @@ const Hero = () => {
 
 export const Introduction = () => {
   return (
-    <div className="flex flex-col items-center h-screen p-12">
+    <div className="flex flex-col items-center h-screen p-4 md:p-12">
       <Hero />
-      <div className="grid grid-cols-3 grid-rows-2 p-12">
+      <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 p-4 md:p-12 gap-1 md:gap-4">
         {features.map((feature) => (
-          <div className="flex items-center justify-start  px-4 py-2">
-            {feature?.icon}
+          <div className="flex items-center justify-center px-4 py-2 border-line border rounded-md">
+            <div className="min-w-6">{feature?.icon}</div>
+
             <div className="ml-4 text-xs leading-6 font-medium text-base-content-2">
               {feature.title}
             </div>
           </div>
         ))}
       </div>
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex items-center gap-4">
+        <Link href="/docs/theme">
+          <Button variant="outline">See Demo</Button>
+        </Link>
         <Link href="/docs/install">
           <Button>Get Started</Button>
         </Link>
