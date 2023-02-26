1c1510b0858d1aa007a404c34b68365c6c98c535
Will <willliuwillliu@gmail.com>
2023-02-26 12:47:38 -0500

    Added cypress

---
+++ apps/docs/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    experimentalStudio: true,
+    baseUrl: "http://localhost:3000",
+  },
+});
